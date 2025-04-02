e6758f0adca652f2e39cd5789ee61b6e0b584554
Curtis <curtisjohn718@aol.com>
2025-04-02 16:05:57 -0400

    completed List Comprehensions and Generator Expressions lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # using a list comprehension write a function return_evens()
+    # return a list of all the even elements of a sequence of integers.
+    # [expression for item in sequence if condition]
+    return [num for num in num_list if num % 2 == 0]
+     
 
 def make_exclamation(sentence_list):
-    pass
+    # using a list comprehension write a function make_exclamation()
+    # takes a list of sentence strings and returns a list of sentence strings with exclamation marks at the end.
+    return [string + "!" for string in sentence_list]
